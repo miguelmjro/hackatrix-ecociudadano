b9f9de980bea7ff07cf132ca1be21aaf57b68b95
dallannunez <dallan.nunez@hypersoft.co>
2019-09-28 10:40:43 -0500

    initial project

---
+++ src/main/java/com/co/latin/ecociudadano/EcociudadanoApplication.java
@@ -0,0 +1,13 @@
+package com.co.latin.ecociudadano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcociudadanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcociudadanoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/latin/ecociudadano/EcociudadanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.latin.ecociudadano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcociudadanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
